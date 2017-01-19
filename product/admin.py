1830b83d3f3f12ed4125a3982f8248e115f479fe
KoIIdun <styopa.plaunov@gmail.com>
2017-01-19 22:51:38 +0400

    Add files via upload

---
+++ product/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product, Size
+# Register your models here.
+
+class SizeAdmin(admin.TabularInline):
+    model = Size
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [SizeAdmin]
+    list_display = ('type', 'manufacturer', 'name', 'cost', 'new', 'sells')
+    list_filter = ['type']
+
+admin.site.register(Product, ProductAdmin)
---
+++ product/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django import forms
+
+# Create your models here.
+
+class Product(models.Model):
+    type = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    cost = models.IntegerField()
+    description = models.TextField(max_length=200)
+    sells = models.BooleanField(default=False)
+    new = models.BooleanField(default=False)
+
+class Size(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.IntegerField()
+    count = models.IntegerField()
