612ec8653c50c305f0572fce3c238719ca692376
KoIIdun <styopa.plaunov@gmail.com>
2017-01-22 20:00:01 +0400

    Title on product page

--- voeucult/settings.py
+++ voeucult/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'general.apps.GeneralCongfig',
+    'general.apps.GeneralConfig',
     'product.apps.ProductConfig',
     'django.contrib.admin',
     'django.contrib.auth',
